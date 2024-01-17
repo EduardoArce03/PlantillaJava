5546b1072050b0d699e6fd0f5d87fc51c4801617
EduardoArce03 <eduardomaldonado2003ortiz@gmail.com>
2024-01-17 18:32:17 -0500

    Primer commit

---
+++ src/main/java/dao/FacturaDAO.java
@@ -0,0 +1,39 @@
+package dao;
+
+import java.util.List;
+
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import model.Factura;
+@Stateless
+public class FacturaDAO {
+	@PersistenceContext
+	private EntityManager em;
+	
+	public void insert(Factura factura) {
+		em.persist(factura);
+	}
+	
+	public void update(Factura factura) {
+		em.merge(factura);
+	}
+	
+	public void remove(int codigo) {
+		Factura factura = em.find(Factura.class, codigo);
+		em.refresh(factura);
+	}
+	
+	public Factura read(int codigo) {
+		Factura factura = em.find(Factura.class, codigo);
+		return factura;
+	}
+	
+	public List<Factura> getAll(){
+		String jpql = "SELECT c FROM Factura c";
+		Query q = em.createQuery(jpql, Factura.class);
+		return q.getResultList();
+	}
+	
+}
